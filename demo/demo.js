885972755df9aa2e8ce93d88f9ca4d0784fa8409
Daria Tkachuk <daryasmail@gmail.com>
2023-03-22 00:20:11 +0200

    Gthub screens in different browsers

---
+++ demo/demo.js
@@ -0,0 +1,14 @@
+const {webkit, chromium, firefox} = require('playwright');
+
+(async () => {
+    for (const browserType of [webkit, firefox, chromium]) {
+      const browser = await browserType.launch();         // запуск каждого браузера
+      const page = await browser.newPage();                 // откроем страницу
+      await page.goto('https://github.com/microsoft/playwright');  // данного сайта
+      await page.screenshot({                                   
+        path: `screenshot-${browserType.name()}.png`,  // сохраняем скриншот 
+      });
+      await browser.close();                                         // закрываем браузер 
+      console.log('success: ' + browserType.name()); // чтобы отследить прогресс
+    };
+  } )();
