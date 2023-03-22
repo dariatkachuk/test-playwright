4dd68651147541838f123abaa5251373bb477d4c
Daria Tkachuk <daryasmail@gmail.com>
2023-03-22 23:40:41 +0200

    Mobile and geolocation tests added

---
+++ demo/demo-geolocation.js
@@ -0,0 +1,16 @@
+const {webkit, chromium, firefox, devices} = require('playwright'); 
+
+(async () => {
+    const browser = await chromium.launch({
+      headless: false,
+    });
+    const context = await browser.newContext({
+        geolocation: {
+            latitude: 51.508076,
+            longitude: -0.0993827,
+          },
+          permissions: ['geolocation']
+    });                                                                             
+    const page = await context.newPage();                
+    await page.goto('https://maps.google.com'); 
+  } )();
---
+++ demo/demo-mobile.js
@@ -0,0 +1,13 @@
+const {webkit, chromium, firefox, devices} = require('playwright'); 
+const iPhone11Pro = devices['iPhone 11 Pro'];
+
+(async () => {
+    const browser = await webkit.launch({
+      headless: false,
+    });
+    const context = await browser.newContext({     //создание контекста
+      ...iPhone11Pro,
+    });                                                                             
+    const page = await context.newPage();            //создание страницы в контексте     
+    await page.goto('https://github.com/microsoft/playwright'); 
+  } )();
